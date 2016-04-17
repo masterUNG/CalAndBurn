c082453bc269c46bc8201c5684d9f5e05718ae03
masterUNG <phrombutr@gmail.com>
2016-04-17 17:38:10 +0700

    วันที่ไม่ซ้ำแว้ววว

--- app/src/main/java/com/example/admin/calandburn/HisFoodData.java
+++ app/src/main/java/com/example/admin/calandburn/HisFoodData.java
@@ -5,19 +5,16 @@ import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
-import android.view.View;
-import android.widget.Button;
-import android.widget.ImageView;
-import android.widget.ListView;
-import android.widget.TextView;
 
-import java.text.SimpleDateFormat;
-import java.util.Calendar;
+import java.util.ArrayList;
 
 /**
  * Created by Admin on 2/4/2559.
  */
 public class HisFoodData extends AppCompatActivity {
+
+    //Explicit
+    private String[] dateAllStrings, dateStrings; // dateAll เวลาทีี่ซ้ำได้
 
 
     @Override
@@ -25,16 +22,55 @@ public class HisFoodData extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.his_food_data);
 
+        //Read All SQLite
+        readAllSQLite();
+
+    }   // Main Method
+
+    private void readAllSQLite() {
+
+        SQLiteDatabase sqLiteDatabase = openOrCreateDatabase(MyOpenHelper.DATABASE_NAME,
+                MODE_PRIVATE, null);
+        Cursor cursor = sqLiteDatabase.rawQuery("SELECT * FROM " + MyManage.table_calary, null);
+        cursor.moveToFirst();
+        int intCount = cursor.getCount();
+
+        dateAllStrings = new String[intCount];
+        String[] foodStrings = new String[intCount];
+        String[] amountStrings = new String[intCount];
+        String[] calFoodStrings = new String[intCount];
+
+        for (int i=0;i<intCount;i++) {
+            dateAllStrings[i] = cursor.getString(1);
+            cursor.moveToNext();
+        }   // for
+        cursor.close();
+
+        dateStrings = findDate(dateAllStrings);
+
+
+    }   // readAllSQLite
+
+    private String[] findDate(String[] dateAllStrings) {
+
+        ArrayList<String> stringArrayList = new ArrayList<String>();
+        for (int i=0;i<dateAllStrings.length;i++) {
+            stringArrayList.add(dateAllStrings[i]);
+        }   // for
+
+        Object[] objects = stringArrayList.toArray();
+        for (Object myObj : objects) {
+
+            if (stringArrayList.indexOf(myObj) != stringArrayList.lastIndexOf(myObj)) {
+                stringArrayList.remove(stringArrayList.lastIndexOf(myObj));
+            }   // if
+
+        }   // for
+
+        Log.d("17April", "ArrayList ==> " + stringArrayList);
+
+        return new String[0];
     }
 
-
-
-
-
-    public void onMainMenuClick(View v) {
-        //Intent i = new Intent(getApplicationContext(),MainActivity.class);
-        //startActivity(i);
-        this.finish();
-    }
 
 } // Main Class
